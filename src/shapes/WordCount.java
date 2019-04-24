225c971f457da7c4e09ac0c401a8db1c3d68cbdb
mareckiw <Haslodogit-1997>
2019-04-25 00:14:03 +0200

    Updated gitignore
    (cherry picked from commit 332f9547bc04127bd5ae2fd9a10d1e334222a743)

---
+++ src/shapes/WordCount.java
@@ -0,0 +1,41 @@
+package shapes;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Collection;
+
+public class WordCount {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String OUT_PATH = "C:/Users/Marek/Desktop/Computer Programming/dok1.txt";
+        File dok1 = new File(OUT_PATH);
+
+        Map m1 = new HashMap();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(dok1))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] words = line.split(" ");//wyjęcie poszczególnych wyrazów
+                for (int i = 0; i < words.length; i++) {
+                    if (m1.get(words[i]) == null) {
+                        m1.put(words[i], 1);
+                    } else {
+                        int newValue = Integer.valueOf(String.valueOf(m1.get(words[i])));
+                        newValue++;
+                        m1.put(words[i], newValue);
+                    }
+                }
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+        }
+        Map <String,String> sorted = new TreeMap<String,String>(m1);
+        for (Object key : sorted.keySet()) {
+            System.out.println("Wyraz:" + key + " Ilość:" + m1.get(key));
+        }
+    }
+}
